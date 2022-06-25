00d9d1cfabc7039ed126f034367fe9ec5796aa66
temur007222 <temurxaydarov1@gmail.com>
2022-06-25 14:05:58 +0500

    Initial commit

---
+++ app/src/main/java/com/example/a494j/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a494j;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
